3cdbaa06e55f041b579e68de3aad047140f5227c
Vinicius-b-q <borbaqueirozvinicius@gmail.com>
2022-10-05 15:50:41 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css';
+
+import { useContext, useEffect } from 'react';
+import { QuizContext } from './context/quiz';
+
+import Welcome from './components/Welcome';
+import Question from './components/Question';
+import GameOver from './components/GameOver';
+
+function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({ type: 'REORDER_QUESTIONS' });
+  }, []);
+  return (
+    <div className="App">
+      <h1>
+        <span>Quiz de</span>
+        <span>Programação</span>
+      </h1>
+      {quizState.gameStage === 'Start' && <Welcome />}
+      {quizState.gameStage === 'Playing' && <Question />}
+      {quizState.gameStage === 'End' && <GameOver />}
+    </div>
+  );
+}
+
+export default App;
